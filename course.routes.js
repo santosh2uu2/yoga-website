0e01a816dfda7c001c20831300c46a9ae248b901
santosh2uu2 <150140465+santosh2uu2@users.noreply.github.com>
2024-10-16 07:40:07 +0530

    Delete course.routes.js

--- course.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/course.routes.js
-import express from 'express';
-import { getCourses } from '../controllers/course.controller.js';
-
-const router = express.Router();
-
-router.get('/', getCourses);
-
-export default router;
