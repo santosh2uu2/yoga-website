46765462947a6830302867a30f59dff82b4fbaf2
santosh2uu2 <150140465+santosh2uu2@users.noreply.github.com>
2024-10-16 07:39:51 +0530

    Delete buy.route.js

--- buy.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createBuy } from "../controller/buy.controller.js";
-
-const router = express.Router();
-
-router.post("/", createBuy);
-
-export default router;
